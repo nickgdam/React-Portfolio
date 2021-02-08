9b932f4c5e281ed740caf633ae857cf15a7a7515
Nick Damario <nicholasgd@comcast.net>
2021-02-08 14:42:19 -0500

    functional about page content (still needs work)

---
+++ src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function content(props) {
+    return (
+        <div>
+            <Container fluid={true}>
+                <Row className="justify-content-center">
+                    <Col md={8}>
+                    {props.children}
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default content
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
 
 function AboutPage(props) {
     return (
         <div>
-            Aboutpage Works!
+            <Hero title={props.title} />
+            <Content>
+            Greetings! My Name is Nick Damario and I am learning web design! I'm 23 and currently living in Winchester, Virginia. I grew up in Southern Maryland and graduated from Northern High school in 2015. Since Graduation I've bounced around from warehouse to retail jobs but now I'm shifting my focus on a sustainable carreer change in Web Design!
+            </Content>
+
         </div>
     )
 }
