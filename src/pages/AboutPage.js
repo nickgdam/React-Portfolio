8ba964a2c8e65991cc2f6b4bde0c8eae2e990924
Nick Damario <nicholasgd@comcast.net>
2021-04-28 13:20:53 -0400

    updating css and about me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Content from '../components/Content';
-import Resume from '../assets/nickresume2020.pdf'
+import Image1 from '../assets/image-1.jpg';
 
 function AboutPage(props) {
     return (
         <div className='bear'>
             <Hero title={props.title} />
             <Content >
-            Greetings! My Name is Nick Damario and I am learning web design! I'm 23 and currently living in Annapolis, Maryland.  I grew up in Southern Maryland and graduated from Northern High school in 2015. Since Graduation I've bounced around from warehouse to retail jobs but now I'm shifting my focus on a sustainable carreer change in Web Design!
-            <p><a href="assets/nickresume2020.pdf">Resume</a></p>
+            Welcome to my page!  My Name is Nick Damario and my focus is full stack web development.  I graduated from Johns Hopkins University Full Stack Coding Bootcamp and I'm currntly looking to land an entry level job, where I would be able to continue working on my skills and adding to my resume.  Since Graduating from highschool I've worked a few retail and warehouse jobs, but now I'm shifting my focus on a starting a new sustainable carreer in Web Developement.  
+            <p><a target="_blank" href="https://docs.google.com/document/d/1RbAiQSEeRK7YalVLxs3tqAVkrLztowV120E1OEIvivU/edit?usp=sharing">Resume</a></p>
+            <img src={Image1}/>
+
             </Content>
 
         </div>
