01e4abc820f47ff89be81fd93a9cc2612c0094cf
Nick Damario <nicholasgd@comcast.net>
2021-05-12 19:30:35 -0400

    making portfolio responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import CardInfo from "../components/CardInfo"
 function Card(props) {
     return (
         <div className="d-inline-block nick-card" onClick={(e) => props.click(props.items)}>
-            <img className="nick-card-image" src={props.item.imgSrc} alt={props.item.imgSrc}/>
+            <img className="nick-card-image yeeter" src={props.item.imgSrc} alt={props.item.imgSrc}/>
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
         </div>
     )
