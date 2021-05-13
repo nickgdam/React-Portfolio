0bc5a238ddce30cdf1cac2ee2e518c63d7aca59e
Nick Damario <nicholasgd@comcast.net>
2021-05-13 15:43:54 -0400

    fixing typo

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,7 +8,7 @@ function AboutPage(props) {
         <div className='bear'>
             <Hero title={props.title} subtitle={props.subtitle}/>
             <Content >
-            Welcome to my page!  My Name is Nick Damario and my focus is full stack web development.  I graduated from Johns Hopkins University Full Stack Coding Bootcamp and I'm currntly looking to land an entry level job, where I would be able to continue working on my skills and adding to my resume.  Since Graduating from highschool I've worked a few retail and warehouse jobs, but now I'm shifting my focus on a starting a new sustainable carreer in Web Developement.  
+            Welcome to my page!  My Name is Nick Damario and my focus is full stack web development.  I graduated from Johns Hopkins University Full Stack Coding Bootcamp and I'm currntly looking to land an entry level job, where I would be able to continue working on my skills and adding to my resume.  Since Graduating from highschool I've worked a few retail and warehouse jobs, but now I'm shifting my focus on a starting a new sustainable career in Web Developement.  
             <p><a target="_blank" href="https://docs.google.com/document/d/1RbAiQSEeRK7YalVLxs3tqAVkrLztowV120E1OEIvivU/edit?usp=sharing">Resume</a></p>
             <img className='yeeter' src={Image1}/>
 
