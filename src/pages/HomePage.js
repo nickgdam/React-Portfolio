64da984e1c64aa5169d19c28df23d292518ff9b2
Nick Damario <nicholasgd@comcast.net>
2021-05-13 15:42:04 -0400

    fixing portfolio styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hero from '../components/Hero';
 import Carousel from "../components/Carousel";
+import Aos from "aos";
 
 function HomePage(props) {
+    useEffect(() => {
+        Aos.init({ duration: 1000});
+    }, []);
     return (
-        <div className="bear">
+        <div   className="bear">
             <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
             <Carousel/>
+
         </div>
     )
 }
